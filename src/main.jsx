4c023c9e566837b145b5123eb6a2528a1d5ce648
Pavan <pavankumarsahu2000@gmail.com>
2025-09-01 18:53:23 +0530

    404.html handled

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import App from "./App";
 import { AuthProvider } from "./AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import "nprogress/nprogress.css";
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, null, redirect);
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  
   <Router basename="/Mandir-Darshan">
     <AuthProvider>
       <App />
