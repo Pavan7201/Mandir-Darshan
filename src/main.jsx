bf987d3066587e3f2a0bc05d72c85a54c6cde7e6
Pavan <pavankumarsahu2000@gmail.com>
2025-09-01 19:03:30 +0530

    updated loading effect

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
+import {Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import "nprogress/nprogress.css";
+import TempleLoader from "./loader/TempleLoader";
+
 const redirect = sessionStorage.redirect;
 if (redirect) {
   delete sessionStorage.redirect;
@@ -13,7 +16,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   
   <Router basename="/Mandir-Darshan">
     <AuthProvider>
+      <Suspense fallback={<TempleLoader />}>
       <App />
+      </Suspense>
     </AuthProvider>
   </Router>
 );
